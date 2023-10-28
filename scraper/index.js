43e91538062540d8e36668b763bf72bfcf46d294
DanielaParra <dparralp396@gmail.com>
2023-10-27 22:46:35 -0400

    rm comment

--- scraper/index.js
+++ scraper/index.js
@@ -6,7 +6,6 @@ const start = async () => {
 	const browser = await puppeteer.launch({ headless: 'new' });
 	const page = await browser.newPage();
 	await page.goto('https://stardewvalleywiki.com/Foraging');
-	// await page.screenshot({ path: 'test.png', fullPage: true });
 
 	const springForage = await getSeasonTableData(page, 'Spring');
 	const summerForage = await getSeasonTableData(page, 'Summer');
