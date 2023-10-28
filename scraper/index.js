b3b7dce09b9620b6f93b7db2f9906ba50c7eb2ab
DanielaParra <dparralp396@gmail.com>
2023-10-27 22:13:10 -0400

    Update w/ 4 season data

--- scraper/index.js
+++ scraper/index.js
@@ -8,7 +8,20 @@ const start = async () => {
 	await page.goto('https://stardewvalleywiki.com/Foraging');
 	// await page.screenshot({ path: 'test.png', fullPage: true });
 
-	await getBaseWikiData(page, 'Fall');
+	const springForage = await getBaseWikiData(page, 'Spring');
+	const summerForage = await getBaseWikiData(page, 'Summer');
+	const fallForage = await getBaseWikiData(page, 'Fall');
+	const winterForage = await getBaseWikiData(page, 'Winter');
+
+	const allForage = [
+		...springForage,
+		...summerForage,
+		...fallForage,
+		...winterForage,
+	];
+
+	// The arguments "null" and "2" are for formatting the JSON output
+	await fs.writeFile('baseGameForage.json', JSON.stringify(allForage, null, 2));
 
 	await browser.close();
 };
