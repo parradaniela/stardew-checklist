6144a87bcc34cbcea83b602bf726f026fb49db99
DanielaParra <dparralp396@gmail.com>
2023-11-02 23:00:34 -0400

    Add middleware, logger, nodemon

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,36 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const path = require('path');
 const corsOptions = require('./config/corsOptions');
 const connectDB = require('./config/dbConnection');
-const PORT = process.env.PORT;
+const { logger, logEvents } = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
+const app = express();
+const PORT = process.env.PORT || 3500;
 
 connectDB();
+app.use(logger);
 app.use(cors(corsOptions));
 app.use(express.json());
+//Can create a public folder to add css if wanted
+app.use('/', express.static(path.join(__dirname, '/public')));
+app.use('/', require('./routes/root'));
 app.use('/Forage', require('./routes/forageRoutes'));
+
+app.all('*', (req, res) => {
+	res.status(404);
+	if (req.accepts('html')) {
+		res.sendFile(path.join(__dirname, 'views', '404.html'));
+	} else if (req.accepts('json')) {
+		res.json({ message: '404 Not Found' });
+	} else {
+		res.type('txt').send('404 Not Found');
+	}
+});
+
+app.use(errorHandler);
 
 mongoose.connection.once('open', () => {
 	console.log('Connected to MongoDB');
@@ -19,4 +39,8 @@ mongoose.connection.once('open', () => {
 
 mongoose.connection.on('error', err => {
 	console.log('Error connecting to the database: ', err);
+	logEvents(
+		`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+		'mongoErrLog.log'
+	);
 });
