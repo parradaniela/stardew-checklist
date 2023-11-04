d9e817080c5dced5178a92098aef367a499c3862
DanielaParra <dparralp396@gmail.com>
2023-11-03 21:54:49 -0400

    Add required to Model

--- backend/models/Forage.js
+++ backend/models/Forage.js
@@ -1,13 +1,38 @@
 const mongoose = require('mongoose');
 
 const forageSchema = new mongoose.Schema({
-	name: String,
-	imgUrl: String,
-	wikiUrl: String,
-	locations: [String],
-	seasons: [String],
-	yearOneAvail: Boolean,
-	game: String,
+	name: {
+		type: String,
+		required: true,
+	},
+	imgUrl: {
+		type: String,
+		required: true,
+	},
+	wikiUrl: {
+		type: String,
+		required: true,
+	},
+	locations: [
+		{
+			type: String,
+			required: true,
+		},
+	],
+	seasons: [
+		{
+			type: String,
+			required: true,
+		},
+	],
+	yearOneAvail: {
+		type: Boolean,
+		required: true,
+	},
+	game: {
+		type: String,
+		required: true,
+	},
 });
 
 module.exports = mongoose.model('Forage', forageSchema, 'forage');
