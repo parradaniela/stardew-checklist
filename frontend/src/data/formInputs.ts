2806e1456eccd44de4d9e09d7bcf25938bf8a4f0
DanielaParra <dparralp396@gmail.com>
2023-11-12 19:09:42 -0500

    Add types and data files

---
+++ frontend/src/data/formInputs.ts
@@ -0,0 +1,49 @@
+import { radioAttributeType } from "../types/types";
+
+export const gameRadioAttributes: radioAttributeType[] = [
+    {
+        label: "Base Game",
+        name: "game",
+        id: "base",
+        value: "base",
+    },
+    {
+        label: "Stardew Valley Expanded",
+        name: "game",
+        id: "sve",
+        value: "sve",
+    },
+    {
+        label: "Ridgeside Village",
+        name: "game",
+        id: "rsv",
+        value: "rsv",
+    },
+];
+
+export const seasonRadioAttributes: radioAttributeType[] = [
+    {
+        label: "Spring",
+        name: "season",
+        id: "spring",
+        value: "Spring",
+    },
+    {
+        label: "Summer",
+        name: "season",
+        id: "summer",
+        value: "Summer",
+    },
+    {
+        label: "Fall",
+        name: "season",
+        id: "fall",
+        value: "Fall",
+    },
+    {
+        label: "Winter",
+        name: "season",
+        id: "winter",
+        value: "Winter",
+    },
+];
---
+++ frontend/src/types/types.ts
@@ -0,0 +1,13 @@
+export type ForageObj = {
+    name: string;
+    imgUrl: string;
+    wikiUrl: string;
+    locations: string[];
+};
+
+export type radioAttributeType = {
+    label: string;
+    name: string;
+    id: string;
+    value: string;
+};
