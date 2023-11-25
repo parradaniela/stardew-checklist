74c9570b9e8fd2cb220903ee2ee9a870dddd0513
DanielaParra <dparralp396@gmail.com>
2023-11-25 14:04:53 -0500

    Update with save destination and err handling

--- scraper/util/writeJsonFile.js
+++ scraper/util/writeJsonFile.js
@@ -1,7 +1,20 @@
-const fs = require('fs/promises');
+const fs = require('fs').promises; // Make sure to use the promises version of 'fs' for async/await
+const path = require('path');
 
 const writeJsonFile = async (fileName, jsonData) => {
-	await fs.writeFile(fileName, JSON.stringify(jsonData, null, 2));
+	const directory = 'jsonOutput';
+	const filePath = path.join(directory, fileName);
+
+	try {
+		// Check if the directory exists, create it if it doesn't
+		await fs.mkdir(directory, { recursive: true });
+
+		// Write the JSON data to a file in the specified directory
+		await fs.writeFile(filePath, JSON.stringify(jsonData, null, 2));
+		console.log(`File written to ${filePath}`);
+	} catch (err) {
+		console.error('Error writing JSON file:', err);
+	}
 };
 
 module.exports = writeJsonFile;
