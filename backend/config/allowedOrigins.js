30645651ffb79660d364f7f528de117c5bca2255
DanielaParra <dparralp396@gmail.com>
2024-02-06 16:53:04 -0500

    allowlist update

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,8 @@
 const allowedOrigins = [
 	'http://localhost:5173',
 	'https://dev-stardew-catalogue.netlify.app/',
+	'https://dev--dev-stardew-catalogue.netlify.app',
+	'https://main--stardew-catalogue.netlify.app/',
 	'https://stardew-catalogue.netlify.app/',
 ];
 
