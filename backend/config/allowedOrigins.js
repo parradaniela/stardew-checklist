1d147718b63acdbf640557ba822c4c4cd25b6cbc
DanielaParra <dparralp396@gmail.com>
2024-02-06 19:36:35 -0500

    tweak allowlist

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,9 +1,9 @@
 const allowedOrigins = [
 	'http://localhost:5173',
-	'https://dev-stardew-catalogue.netlify.app/',
+	'https://dev-stardew-catalogue.netlify.app',
 	'https://dev--dev-stardew-catalogue.netlify.app',
-	'https://main--stardew-catalogue.netlify.app/',
-	'https://stardew-catalogue.netlify.app/',
+	'https://main--stardew-catalogue.netlify.app',
+	'https://stardew-catalogue.netlify.app',
 ];
 
 module.exports = allowedOrigins;
