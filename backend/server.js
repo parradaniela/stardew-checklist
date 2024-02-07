c830b77c2802817fe3c8415f6b9280e60ece9efc
DanielaParra <dparralp396@gmail.com>
2024-02-06 19:04:44 -0500

    Uncomment for dev branch

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use(errorHandler);
 
 mongoose.connection.once('open', () => {
 	console.log('Connected to MongoDB');
-	// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+	app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
 
 mongoose.connection.on('error', err => {
