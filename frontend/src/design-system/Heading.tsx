f304aed01c40df3ace57f9e76e5f07fe843f7402
DanielaParra <dparralp396@gmail.com>
2024-02-19 17:07:35 -0500

    Update About

---
+++ frontend/src/design-system/Heading.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react'
+
+type Props = {
+    children: ReactNode
+}
+
+const Heading = ({ children }: Props) => {
+    return (
+        <h2 className="font-stardew text-5xl text-stardew_dark_brown">
+            {children}
+        </h2>
+    )
+}
+
+export default Heading
---
+++ frontend/src/design-system/Paragraph.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react"
+
+type Props = {
+    children: ReactNode
+}
+
+const Paragraph = ({ children }: Props) => {
+    return (
+        <p className='text-2xl'>
+            {children}
+        </p>
+    )
+}
+
+export default Paragraph
